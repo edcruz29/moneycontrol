893ae601e00c274c63d72951db6024503e65771e
Eduardo <galvaocruz16@gmail.com>
2021-11-29 20:46:20 -0300

    consumo da api, criação do modal e estilização

---
+++ README.md
@@ -0,0 +1,24 @@
+# Money Control
+
+## Sobre
+
+Ferramenta para controle de entradas e saídas de transações financeiras.
+
+## Funcionalidades
+
+- **Cadastro de nova transação**
+- **Exclusão de transação**
+- **Atualização de transação**
+- **Cálculo**
+
+## Ferramentas e Tecnologias
+
+- **Typescript**
+- **Componentização**
+- **Styled-Components**
+- **Axios**
+- **React-Modal**
+- **Reactjs - Create App**
+
+**Desenvolvido por:**
+**[Eduardo Cruz](https://github.com/edcruz29/).**# Desafio 1 - Rocketseat Ignite
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,13 +2,21 @@ import logoImg from "../../assets/logo.svg";
 import { Container } from "./styles";
 import { Content } from "./styles";
 
-export function Header() {
+interface HeaderProps{
+  onOpenNewTransactionModal:()=>void;
+}
+
+export function Header({onOpenNewTransactionModal}:HeaderProps) {
+  
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="money control" />
-        <button type="button">Nova Transação</button>
+        <button type="button" onClick={onOpenNewTransactionModal}>
+          Nova Transação
+        </button>
       </Content>
+      
     </Container>
   );
 }
